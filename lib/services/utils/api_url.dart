6c261adb0375a331dc60ef4765c62308d63f038f
Sumit-9900 <paulsumit9900@gmail.com>
2024-07-04 08:27:30 +0530

    .env

--- lib/services/utils/api_url.dart
+++ lib/services/utils/api_url.dart
@@ -1,4 +1,6 @@
-const String appid = 'b2199a6312783d7cff8cece935a83692';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+final appid = dotenv.env['appid'];
 
 String baseurl =
     'https://api.openweathermap.org/data/2.5/weather?q=Siliguri&appid=$appid&units=metric';
